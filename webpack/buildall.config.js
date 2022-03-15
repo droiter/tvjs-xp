132f59a45a82182a5bb2d9527871b025d4c8a520
droiter <android_openwrt@sina.com>
2022-03-16 01:33:21 +0800

    webpack minify

--- webpack/buildall.config.js
+++ webpack/buildall.config.js
@@ -56,10 +56,27 @@ module.exports = {
     optimization: {
         minimize: true,
         minimizer: [new TerserPlugin({
-            include: /\.min\.js$/,
-            extractComments: {
-                banner: BANNER
-            }
+        //include: /.js$/,
+        //sourceMap: false,
+        //cache: true,
+        parallel: true,
+        terserOptions: {
+          ecma: undefined,
+          warnings: false,
+          parse: {},
+          compress: {},
+          mangle: true, // Note `mangle.properties` is `false` by default.
+          module: false,
+          output: null,
+          toplevel: false,
+          nameCache: null,
+          ie8: false,
+          keep_classnames: undefined,
+          keep_fnames: false,
+          safari10: false,
+        },
+
+        extractComments: 'all'
         })]
     },
     devtool: 'source-map',
